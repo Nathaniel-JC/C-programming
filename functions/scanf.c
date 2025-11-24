5f4eec62c1eca1ed02be81d7092c331a0a2da95e
Nathaniel-JC <jithrochakraborty@gmail.com>
2025-11-24 20:17:54 +0600

    Add files via upload

---
+++ functions/scanf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int n;
+ 
+    printf("give me your age");
+    scanf("%d",&n);
+
+    printf("your is :%d",n);
+    
+    printf("hi bro, debug");
+
+    return 0;
+}
